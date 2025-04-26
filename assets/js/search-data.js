4f58a576ce6fadbb73838f4e17ba033ff7d1bd0a
brindagokul <48494689+brindagokul@users.noreply.github.com>
2025-04-26 19:24:10 +0000

    Deploying to gh-pages from @ brindagokul/brindagokul.github.io@1eb1f39a68f94ec3fe1b1c0e3916f7925aede68b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "dropdown-bookshelf",
+  },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
               section: "Dropdown",
